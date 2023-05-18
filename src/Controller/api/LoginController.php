c91b7769fb1e8de56611bf073cb6ff4f9af89768
bwikp <zilpa.michel@gmail.com>
2023-05-18 19:15:44 +0200

    d

--- src/Controller/api/LoginController.php
+++ src/Controller/api/LoginController.php
@@ -1,24 +1,24 @@
 <?php
 
-namespace App\Controller\api;
+namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-
-class LoginController extends  AbstractController
-{                          
-    /**
-     * @throws \JsonException
-     */
-    #[Route(path: '/api/login', name: 'api_login', methods: ["POST"])]
-
-     public function ApiLogin()
+class LoginController extends AbstractController
+{
+    #[Route(path:'/api/login', name: 'api_login', methods:["POST"])]
+    public function ApiLogin()
     {
-            $user = $this->getUser();
+        $user = $this->getUser();
 
-            return new JsonResponse(json_encode("logged in",JSON_THROW_ON_ERROR));
+        $userData = [
+            'email'=>$user->getEmail(),
+            'first_name'=>$user->getFirstName(),
+            'last_name'=>$user->getLastName()
+        ];
+
+        return $this->json($userData);
     }
 }
