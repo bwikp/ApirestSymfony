cae180d99e073bf2d3211bb6cdfcc14456b6ee87
Bwikp <zilpa.michel@gmail.com>
2023-05-22 16:57:43 +0200

    OK LETSGO

--- src/Controller/api/RegisterController.php
+++ src/Controller/api/RegisterController.php
@@ -1,26 +1,38 @@
 <?php
 
-namespace App\Controller;
+    namespace App\Controller\Api;
 
+// use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use App\Security\SecurtiryAuthAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\User;
-
+use App\Entity\User;    
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 class RegisterController extends AbstractController
 {
     #[Route('/api/register', name: 'api_register',methods:["POST"])]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, SecurtiryAuthAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(ValidatorInterface $validator,SerializerInterface $serializer ,Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, SecurtiryAuthAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
-
         if($this->getUser())
-        {
+            {
+                return new JsonResponse($serializer->serialize(['message'=>'you must logout into get register page'],'json'),Response::HTTP_UNAUTHORIZED,[],true);
+            }
 
-        }
+            $newUser = $serializer->deserialize($request->getContent(),User::class,'json');
+
+            $error = $validator->validate($newUser);
+
+            if($error->count()>0)
+                {
+                    return new JsonResponse($serializer->serialize($error,'json'),Response::HTTP_BAD_REQUEST);
+                }
 
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
